907df3f4caee1c5ecbe087709d27cd5ff765cfde
Ade <mymail@gmail.comadeobayomi@gmail.com>
2023-01-19 20:02:00 -0500

    draft updates

--- main.js
+++ main.js
@@ -41,202 +41,210 @@ function closedPolygon(...args) {
 /*caterpillar cartoon*/
 
 /*bugs head component*/
-let bugHead = drawing.append('ellipse')
+let BugHead = drawing.append('ellipse')
     .attr("cx", 80)
-    .attr("cy",295)
+    .attr("cy", 295)
     .attr("rx", 30)
-    .attr("ry",30)
-    .attr("fill", "lightgreen");
+    .attr("ry", 30)
+    /*the following hex code is for a light green color*/
+    .attr("fill", "#B1F5A7");
 
 /*bug body*/
-let bugBody1 = drawing.append('ellipse')
+let BugBody1 = drawing.append('ellipse')
     .attr("cx", 130)
-    .attr("cy",310)
+    .attr("cy", 310)
     .attr("rx", 30)
-    .attr("ry",30)
-    .attr("fill", "green");
+    .attr("ry", 30)
+    .attr("fill", "#5DB87D");
 
-let bugBody2 = drawing.append('ellipse')
+let BugBody2 = drawing.append('ellipse')
     .attr("cx", 160)
-    .attr("cy",280)
+    .attr("cy", 280)
     .attr("rx", 30)
-    .attr("ry",30)
-    .attr("fill", "lightgreen");
+    .attr("ry", 30)
+    /*the following hex code is for a light green color*/
+    .attr("fill", "#B1F5A7");
 
-let bugBody3 = drawing.append('ellipse')
+
+let BugBody3 = drawing.append('ellipse')
     .attr("cx", 200)
     .attr("cy", 310)
     .attr("rx", 30)
-    .attr("ry",30)
-    .attr("fill", "green");
+    .attr("ry", 30)
+    .attr("fill", "#5DB87D");
 
-let bugBody4 = drawing.append('ellipse')
+let BugBody4 = drawing.append('ellipse')
     .attr("cx", 235)
     .attr("cy", 300)
     .attr("rx", 30)
-    .attr("ry",30)
-    .attr("fill", "lightgreen");
+    .attr("ry", 30)
+    /*the following hex code is for a light green color*/
+    .attr("fill", "#B1F5A7");
 
-let bugBody5 = drawing.append('ellipse')
+let BugBody5 = drawing.append('ellipse')
     .attr("cx", 263)
     .attr("cy", 270)
     .attr("rx", 30)
-    .attr("ry",30)
-    .attr("fill", "green");
+    .attr("ry", 30)
+    .attr("fill", "#5DB87D");
 
-let bugBody6 = drawing.append('ellipse')
+let BugBody6 = drawing.append('ellipse')
     .attr("cx", 295)
     .attr("cy", 290)
     .attr("rx", 30)
-    .attr("ry",30)
-    .attr("fill", "lightgreen");
+    .attr("ry", 30)
+    /*the following hex code is for a light green color*/
+    .attr("fill", "#B1F5A7");
 
-let bugBody7 = drawing.append('ellipse')
+let BugBody7 = drawing.append('ellipse')
     .attr("cx", 325)
     .attr("cy", 315)
     .attr("rx", 30)
-    .attr("ry",30)
-    .attr("fill", "green");
+    .attr("ry", 30)
+    .attr("fill", "#5DB87D");
 
-let bugBody8 = drawing.append('ellipse')
+let BugBody8 = drawing.append('ellipse')
     .attr("cx", 355)
     .attr("cy", 295)
     .attr("rx", 20)
-    .attr("ry",20)
-    .attr("fill", "lightgreen");
+    .attr("ry", 20)
+    /*the following hex code is for a light green color*/
+    .attr("fill", "#B1F5A7");
 
-let bugBody9 = drawing.append('ellipse')
+let BugBody9 = drawing.append('ellipse')
     .attr("cx", 370)
     .attr("cy", 280)
     .attr("rx", 15)
-    .attr("ry",15)
-    .attr("fill", "green");
+    .attr("ry", 15)
+    .attr("fill", "#5DB87D");
 
 
 
 /*bugs antennae*/
-let bugAntenna = drawing.append ('line')
+let BugAntenna = drawing.append('line')
     .attr("x1", 55)
-    .attr("y1",250)
-    .attr("x2",65)
-    .attr("y2",268.5)
+    .attr("y1", 250)
+    .attr("x2", 65)
+    .attr("y2", 268.5)
     .attr("stroke", "black")
-    .attr('stroke-width',.75);
+    .attr('stroke-width', .75);
 
-let bugAntenna2 = drawing.append ('line')
+let BugAntenna2 = drawing.append('line')
     .attr("x1", 55)
-    .attr("y1",250)
-    .attr("x2",30)
-    .attr("y2",250)
+    .attr("y1", 250)
+    .attr("x2", 30)
+    .attr("y2", 250)
     .attr("stroke", "black")
-    .attr('stroke-width',.75);
+    .attr('stroke-width', .75);
 
-let bugAntenna3 = drawing.append ('line')
+let BugAntenna3 = drawing.append('line')
     .attr("x1", 80)
-    .attr("y1",270)
-    .attr("x2",70)
-    .attr("y2",250)
+    .attr("y1", 270)
+    .attr("x2", 70)
+    .attr("y2", 250)
     .attr("stroke", "black")
-    .attr('stroke-width',.75);
+    .attr('stroke-width', .75);
 
-let bugAntenna4 = drawing.append ('line')
-.attr("x1",70)
-.attr("y1",250)
-.attr("x2",50)
-.attr("y2",245.5)
-.attr("stroke", "black")
-.attr('stroke-width',.75);
+let BugAntenna4 = drawing.append('line')
+    .attr("x1", 70)
+    .attr("y1", 250)
+    .attr("x2", 50)
+    .attr("y2", 245.5)
+    .attr("stroke", "black")
+    .attr('stroke-width', .75);
 
 
 /*mouth component*/
 
-let bugMouth = drawing.append('line')
+let BugMouth = drawing.append('line')
     .attr("x1", 80)
-    .attr("y1",308)
-    .attr("x2",60)
-    .attr("y2",308)
+    .attr("y1", 308)
+    .attr("x2", 60)
+    .attr("y2", 308)
     .attr("stroke", "black")
     .attr('stroke-width', 2);
 
 
 /*bugs eye component*/
 
-let bugEye = drawing.append('ellipse')
+let BugEyLeft = drawing.append('ellipse')
     .attr("cx", 50)
-    .attr("cy",285)
+    .attr("cy", 285)
     .attr("rx", 5)
-    .attr("ry",5)
+    .attr("ry", 5)
     .attr("fill", "black");
 
-let bugEye2 = drawing.append('ellipse')
+let BugEyeRight = drawing.append('ellipse')
     .attr("cx", 70)
-    .attr("cy",285)
+    .attr("cy", 285)
     .attr("rx", 5)
-    .attr("ry",5)
+    .attr("ry", 5)
     .attr("fill", "black");
 
 /*butterfly cartoon*/
-    
-/*bottom wing component*/
-let bottomWing = drawing.append('polyline')
-    .attr("points", closedPolygon(200,100,200,200,150,100))
-    .attr("transform", "translate(180,20)" )
-    .attr("fill", "orange");
 
-let bottomWing2 = drawing.append('polyline')
-    .attr("points", closedPolygon(100,200,150,100,100,100))
-    .attr("transform", "translate(290,20)" )
-    .attr("fill", "orange");
+/*bottom wing component*/
+let BottomWingLeft = drawing.append('polyline')
+    .attr("points", closedPolygon(200, 100, 200, 200, 150, 100))
+    .attr("transform", "translate(170,20)")
+    /*the following hexcode is the closest orange I could find to the original drawing*/
+    .attr("fill", "#F88D53");
+
+let BottomWingRight = drawing.append('polyline')
+    .attr("points", closedPolygon(100, 200, 150, 100, 100, 100))
+    .attr("transform", "translate(280,20)")
+    /*the following hexcode is the closest color I could find to the original drawing*/
+    .attr("fill", "#F88D53");
 
 /*top wing component*/
-let topWing = drawing.append('polyline')
-    .attr("points", closedPolygon(350,320,300,250,264,348))
-    .attr("transform", "translate(120,-232)" )
+let TopWingRight = drawing.append('polyline')
+    .attr("points", closedPolygon(350, 320, 300, 250, 264, 348))
+    .attr("transform", "translate(110,-232)")
     .attr("fill", "yellow");
 
-let topWing2 = drawing.append('polyline')
-    .attr("points", closedPolygon(250,332,350,350,300,264))
-    .attr("transform", "translate(40,-235.5)" )
+let TopWingLeft = drawing.append('polyline')
+    .attr("points", closedPolygon(250, 332, 350, 350, 300, 264))
+    .attr("transform", "translate(25,-235.5)")
     .attr("fill", "yellow");
 
 
 /*butterfly body component*/
-let butterflyBody = drawing.append('rect')
-    .attr("x", 380)
+let ButterflyBody = drawing.append('rect')
+    .attr("x", 370)
     .attr("y", 60)
     .attr("width", 10)
     .attr("height", 150)
     .attr("fill", "lightgreen");
 
 /*butterfly antennae*/
-let butterflyAntenna= drawing.append('line')
-    .attr("x1", 360)
-    .attr("y1",25)
-    .attr("x2",375)
-    .attr("y2",55)
+let ButterflyAntennaLeft = drawing.append('line')
+    .attr("x1", 350)
+    .attr("y1", 25)
+    .attr("x2", 365)
+    .attr("y2", 50)
     .attr("stroke", "black")
     .attr('stroke-width', .75);
 
-let butterflyAntenna2= drawing.append('line')
+let ButterflyAntennaRight = drawing.append('line')
     .attr("x1", 400)
-    .attr("y1",25)
-    .attr("x2",390)
-    .attr("y2",55)
+    .attr("y1", 25)
+    .attr("x2", 385)
+    .attr("y2", 50)
     .attr("stroke", "black")
     .attr('stroke-width', .75);
 
 /*butterfly eyes*/
-let butterflyEye = drawing.append('ellipse')
-    .attr("cx", 375)
-    .attr("cy",60)
+let ButterflyEyeLeft = drawing.append('ellipse')
+    .attr("cx", 365)
+    .attr("cy", 60)
     .attr("rx", 10)
-    .attr("ry",10)
+    .attr("ry", 10)
     .attr("fill", "red");
 
-let butterflyEye2 = drawing.append('ellipse')
-    .attr("cx", 394)
-    .attr("cy",60)
+let ButterflyEyeRight = drawing.append('ellipse')
+    .attr("cx", 385)
+    .attr("cy", 60)
     .attr("rx", 10)
-    .attr("ry",10)
+    .attr("ry", 10)
     .attr("fill", "red");
 
